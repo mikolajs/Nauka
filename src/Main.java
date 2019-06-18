1fbd1eb6e807101bb9cec0eaed8a8dc4ccc90758
Mikołaj Sochacki <mikolajsochacki@gmail.com>
2019-06-18 17:46:35 +0200

    Poprawione

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 class Main {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         System.out.println("Silnia z " + n + " wynosi: " + silnia(n));
@@ -11,12 +11,11 @@ class Main {
 
     public static BigInteger silnia(long n){
         BigInteger s = new BigInteger("1");
-        byte a = 0b00000010;
         while(n > 1){
             s = s.multiply(BigInteger.valueOf(n));
             n--;
         }
-        
+
         return s;
     }
 }
